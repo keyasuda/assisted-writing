25e1b51fcb423eb584cfa01f28df7996ac5dae81
keyasuda <keyasuda@users.noreply.github.com>
2024-07-18 19:56:50 +0900

    token count on status-bar

---
+++ spec/assisted-writing-view-spec.js
@@ -0,0 +1,7 @@
+'use babel'
+
+import AssistedWritingView from '../lib/assisted-writing-view'
+
+describe('AssistedWritingView', () => {
+  //
+})
---
+++ spec/token-count-worker-spec.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+describe('Web Worker コード', () => {
+  it('トークン数を正しくカウントする', async () => {
+    const worker = new Worker(path.resolve(__dirname, '../lib/token-count-worker.js'));
+
+    await new Promise(resolve => {
+      worker.addEventListener('message', (event) => {
+        const count = event.data;
+
+        expect(count).toBe(2); // テスト対象のテキストのトークン数
+        resolve();
+      });
+
+      worker.postMessage('こんにちは！'); // テスト対象のテキスト
+    });
+  });
+});
