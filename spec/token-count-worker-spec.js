bf122444c63e30d01a9013cd6c4567d19e778424
keyasuda <keyasuda@users.noreply.github.com>
2024-08-10 21:05:47 +0900

    fix test

--- spec/token-count-worker-spec.js
+++ spec/token-count-worker-spec.js
@@ -1,18 +1,23 @@
-const path = require('path');
+const path = require('path')
 
 describe('Web Worker コード', () => {
   it('トークン数を正しくカウントする', async () => {
-    const worker = new Worker(path.resolve(__dirname, '../lib/token-count-worker.js'));
+    const worker = new Worker(
+      path.resolve(__dirname, '../lib/token-count-worker.js')
+    )
 
-    await new Promise(resolve => {
+    await new Promise((resolve) => {
       worker.addEventListener('message', (event) => {
-        const count = event.data;
+        const count = event.data
 
-        expect(count).toBe(2); // テスト対象のテキストのトークン数
-        resolve();
-      });
+        expect(count).toBe(2) // テスト対象のテキストのトークン数
+        resolve()
+      })
 
-      worker.postMessage('こんにちは！'); // テスト対象のテキスト
-    });
-  });
-});
+      worker.postMessage({
+        text: 'こんにちは！', // テスト対象のテキスト
+        model: 'gpt4o',
+      })
+    })
+  })
+})
