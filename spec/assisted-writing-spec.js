61c95645902908e6fdd055cd8b705ff92586b4a4
keyasuda <keyasuda@users.noreply.github.com>
2024-08-12 20:17:55 +0900

    add config to enable/disable each backends

--- spec/assisted-writing-spec.js
+++ spec/assisted-writing-spec.js
@@ -1,6 +1,6 @@
-'use babel';
+'use babel'
 
-import AssistedWriting from '../lib/assisted-writing';
+import AssistedWriting from '../lib/assisted-writing'
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
@@ -8,66 +8,61 @@ import AssistedWriting from '../lib/assisted-writing';
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
 describe('AssistedWriting', () => {
-  let workspaceElement, activationPromise;
+  let workspaceElement, activationPromise
 
   beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('assisted-writing');
-  });
+    workspaceElement = atom.views.getView(atom.workspace)
+    activationPromise = atom.packages.activatePackage('assisted-writing')
+  })
 
-  // describe('when the assisted-writing:toggle event is triggered', () => {
-  //   // it('hides and shows the modal panel', () => {
-  //     // Before the activation event the view is not on the DOM, and no panel
-  //     // has been created
-  //     expect(workspaceElement.querySelector('.assisted-writing')).not.toExist();
-  //
-  //     // This is an activation event, triggering it will cause the package to be
-  //     // activated.
-  //     atom.commands.dispatch(workspaceElement, 'assisted-writing:toggle');
-  //
-  //     waitsForPromise(() => {
-  //       return activationPromise;
-  //     });
-  //
-  //     runs(() => {
-  //       expect(workspaceElement.querySelector('.assisted-writing')).toExist();
-  //
-  //       let assistedWritingElement = workspaceElement.querySelector('.assisted-writing');
-  //       expect(assistedWritingElement).toExist();
-  //
-  //       let assistedWritingPanel = atom.workspace.panelForItem(assistedWritingElement);
-  //       expect(assistedWritingPanel.isVisible()).toBe(true);
-  //       atom.commands.dispatch(workspaceElement, 'assisted-writing:toggle');
-  //       expect(assistedWritingPanel.isVisible()).toBe(false);
-  //     });
-  //   });
-  //
-  //   it('hides and shows the view', () => {
-  //     // This test shows you an integration test testing at the view level.
-  //
-  //     // Attaching the workspaceElement to the DOM is required to allow the
-  //     // `toBeVisible()` matchers to work. Anything testing visibility or focus
-  //     // requires that the workspaceElement is on the DOM. Tests that attach the
-  //     // workspaceElement to the DOM are generally slower than those off DOM.
-  //     jasmine.attachToDOM(workspaceElement);
-  //
-  //     expect(workspaceElement.querySelector('.assisted-writing')).not.toExist();
-  //
-  //     // This is an activation event, triggering it causes the package to be
-  //     // activated.
-  //     atom.commands.dispatch(workspaceElement, 'assisted-writing:toggle');
-  //
-  //     waitsForPromise(() => {
-  //       return activationPromise;
-  //     });
-  //
-  //     runs(() => {
-  //       // Now we can test for view visibility
-  //       let assistedWritingElement = workspaceElement.querySelector('.assisted-writing');
-  //       expect(assistedWritingElement).toBeVisible();
-  //       atom.commands.dispatch(workspaceElement, 'assisted-writing:toggle');
-  //       expect(assistedWritingElement).not.toBeVisible();
-  //     });
-  //   });
-  // });
-});
+  describe('enabledBackends()', () => {
+    it('returns an array of enabled backends', () => {
+      // local APIが有効、Gemini APIが無効の場合
+      atom.config.set('assisted-writing.local.enable', true)
+      atom.config.set('assisted-writing.gemini.enable', false)
+      expect(AssistedWriting.enabledBackends()).toEqual(['local'])
+
+      // local APIが無効、Gemini APIが有効の場合
+      atom.config.set('assisted-writing.local.enable', false)
+      atom.config.set('assisted-writing.gemini.enable', true)
+      expect(AssistedWriting.enabledBackends()).toEqual(['gemini'])
+
+      // local APIとGemini APIが両方有効の場合
+      atom.config.set('assisted-writing.local.enable', true)
+      atom.config.set('assisted-writing.gemini.enable', true)
+      expect(AssistedWriting.enabledBackends()).toEqual(['local', 'gemini'])
+
+      // local APIとGemini APIが両方無効の場合
+      atom.config.set('assisted-writing.local.enable', false)
+      atom.config.set('assisted-writing.gemini.enable', false)
+      expect(AssistedWriting.enabledBackends()).toEqual([])
+    })
+  })
+
+  describe('flipBackend()', () => {
+    it('returns current backend if no backend is enabled', () => {
+      atom.config.set('assisted-writing.local.enable', false)
+      atom.config.set('assisted-writing.gemini.enable', false)
+      expect(AssistedWriting.flipBackend('local')).toEqual('local')
+    })
+
+    it('returns the only enabled backend if only one backend is enabled', () => {
+      // local API only enabled
+      atom.config.set('assisted-writing.local.enable', true)
+      atom.config.set('assisted-writing.gemini.enable', false)
+      expect(AssistedWriting.flipBackend('gemini')).toEqual('local')
+
+      // gemini API only enabled
+      atom.config.set('assisted-writing.local.enable', false)
+      atom.config.set('assisted-writing.gemini.enable', true)
+      expect(AssistedWriting.flipBackend('local')).toEqual('gemini')
+    })
+
+    it('returns the other enabled backend if two backends are enabled', () => {
+      atom.config.set('assisted-writing.local.enable', true)
+      atom.config.set('assisted-writing.gemini.enable', true)
+      expect(AssistedWriting.flipBackend('local')).toEqual('gemini')
+      expect(AssistedWriting.flipBackend('gemini')).toEqual('local')
+    })
+  })
+})
